1efaefd67ac9b9b11cf1b5dfdf6e96df781d527f
Mikewando <3258334+Mikewando@users.noreply.github.com>
2024-03-19 19:27:35 -0400

    yay it works

--- neosr/data/tbc_dataset.py
+++ neosr/data/tbc_dataset.py
@@ -4,7 +4,6 @@ import numpy as np
 from pathlib import Path
 
 UINT16_MAX = 2**16 - 1
-FIELD_HEIGHT = 243
 
 @DATASET_REGISTRY.register()
 class paired_tbc(data.Dataset):
@@ -15,10 +14,6 @@ class paired_tbc(data.Dataset):
         super(paired_tbc, self).__init__()
         self.opt = opt
 
-        if opt['field'] == "top":
-            self.frame_offset = FIELD_HEIGHT
-        else:
-            self.frame_offset = 0
         self.gt_folder = opt['dataroot_gt']
         self.gt_list = sorted(Path(self.gt_folder).glob("*.npy"))
         self.lq_tbc = opt['tbc']
@@ -26,18 +21,23 @@ class paired_tbc(data.Dataset):
     def __getitem__(self, index):
         # Load gt images. Dimension order: CHW; channel order: YUV;
         # image range: [0, 1], float32.
-        img_gt = np.load(self.gt_list[index])[::, self.frame_offset:self.frame_offset + FIELD_HEIGHT:, ::]
+        img_gt = np.load(self.gt_list[index])
 
         # Load lq images. Dimension order: CHW;
         # signal range: [0, 1], float32.
-        img_lq = np.stack([
+        bottom, top = np.split(
             (np.fromfile(
                 self.lq_tbc,
                 dtype=np.uint16,
                 count=910 * 526,
                 offset=index * 910 * 526 * 2).astype(np.float32) / UINT16_MAX
-            ).reshape(526, 910)[39 + self.frame_offset:39 + self.frame_offset + FIELD_HEIGHT:, 147:905:]
-        ])
+        #    ).reshape(526, 910)[39:525:, 147:905:], 2)
+            ).reshape(526, 910), 2)
+        #img_lq = np.zeros((486, 758), dtype=np.float32)
+        img_lq = np.zeros((526, 910), dtype=np.float32)
+        img_lq[0::2] = top
+        img_lq[1::2] = bottom
+        img_lq = np.stack([img_lq[39:525:, 147:905:]])
 
         return {'lq': img_lq, 'gt': img_gt}
 
@@ -53,22 +53,24 @@ class single_tbc(data.Dataset):
     def __init__(self, opt):
         super(single_tbc, self).__init__()
         self.opt = opt
-        if opt['field'] == "top":
-            self.frame_offset = FIELD_HEIGHT
-        else:
-            self.frame_offset = 0
+
         self.lq_tbc = Path(opt['tbc'])
         self.frames = opt['frames']
 
     def __getitem__(self, index):
-        img_lq = np.stack([
+        bottom, top = np.split(
             (np.fromfile(
                 self.lq_tbc,
                 dtype=np.uint16,
                 count=910 * 526,
                 offset=self.frames[index] * 910 * 526 * 2).astype(np.float32) / UINT16_MAX
-            ).reshape(526, 910)[39 + self.frame_offset:39 + self.frame_offset + FIELD_HEIGHT:, 147:905:]
-        ])
+        #    ).reshape(526, 910)[39:525:, 147:905:], 2)
+            ).reshape(526, 910), 2)
+        #img_lq = np.zeros((486, 758), dtype=np.float32)
+        img_lq = np.zeros((526, 910), dtype=np.float32)
+        img_lq[0::2] = top
+        img_lq[1::2] = bottom
+        img_lq = np.stack([img_lq[39:525:, 147:905:]])
         return {'lq': img_lq, 'lq_path': str(self.lq_tbc.with_stem(f'{self.lq_tbc.stem}_frame_{self.frames[index]}'))}
 
     def __len__(self):
