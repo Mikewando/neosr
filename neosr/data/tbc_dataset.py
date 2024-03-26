ad2b1d860622e1f6b267fd24834f07628ffa7dd0
Mikewando <3258334+Mikewando@users.noreply.github.com>
2024-03-26 10:24:12 -0400

    vaguely support either single luma tbc or luma+chroma tbc

--- neosr/data/tbc_dataset.py
+++ neosr/data/tbc_dataset.py
@@ -17,10 +17,13 @@ class paired_tbc(data.Dataset):
 
         self.gt_folder = opt['dataroot_gt']
         self.gt_list = sorted(Path(self.gt_folder).glob("*.npy"))
-        self.lq_tbc = Path(opt['tbc'])
-        if self.lq_tbc.stat().st_size != len(self.gt_list) * 526 * 910 * 2:
-            raise Exception('GT count does not match expected TBC size')
-        with open(self.lq_tbc.with_suffix('.tbc.json')) as tbc_json_file:
+        self.lq_tbcs = [Path(tbc) for tbc in opt['tbc']]
+        for tbc in self.lq_tbcs:
+            if tbc.stat().st_size != len(self.gt_list) * 526 * 910 * 2:
+                raise Exception('GT count does not match expected TBC size')
+        # TODO only the luma will have one, this enforces luma first,
+        # we should be more explicit or flexible I guess
+        with open(self.lq_tbcs[0].with_suffix('.tbc.json')) as tbc_json_file:
             tbc_json = json.load(tbc_json_file)
         self.active_start = tbc_json['videoParameters']['activeVideoStart']
         self.active_end = tbc_json['videoParameters']['activeVideoEnd']
@@ -34,21 +37,62 @@ class paired_tbc(data.Dataset):
         # Each complete frame is 525(+1 empty) rows of 910 samples.
         # The frames are stored with the bottom field stacked on top of
         # the top field. We split the fields and then interleave them TFF.
-        bottom, top = np.split(
-            (np.fromfile(
-                self.lq_tbc,
-                dtype=np.uint16,
-                count=910 * 526,
-                offset=index * 910 * 526 * 2).astype(np.float32) / UINT16_MAX
-            ).reshape(526, 910), 2)
-        # Convert lq input. Dimension order: CHW;
-        # signal range: [0, 1], float32.
+        stacked_lq = []
+        #for lq_tbc in self.lq_tbcs:
+        #    bottom, top = np.split(
+        #        (np.fromfile(
+        #            lq_tbc,
+        #            dtype=np.uint16,
+        #            count=910 * 526,
+        #            offset=index * 910 * 526 * 2).astype(np.float32) / UINT16_MAX
+        #        ).reshape(526, 910), 2)
+        #    # Convert lq input. Dimension order: CHW;
+        #    # signal range: [0, 1], float32.
+        #    img_lq = np.zeros((486, self.active_end - self.active_start), dtype=np.float32)
+        #    img_lq[0::2] = top[19:262:, self.active_start:self.active_end:]
+        #    img_lq[1::2] = bottom[20::, self.active_start:self.active_end:]
+        #    stacked_lq.append(img_lq)
+        if len(self.lq_tbcs) == 2:
+            luma_tbc, chroma_tbc = self.lq_tbcs
+        else:
+            luma_tbc, = self.lq_tbcs
+            chroma_tbc = None
+
+        src = np.fromfile(
+            luma_tbc,
+            dtype=np.uint16,
+            count=910 * 526,
+            offset=index * 910 * 526 * 2
+        ).astype(np.float32) / UINT16_MAX
+        src = src.reshape(526, 910)
+
+        bottom, top = np.split(src, 2)
+
         img_lq = np.zeros((486, self.active_end - self.active_start), dtype=np.float32)
         img_lq[0::2] = top[19:262:, self.active_start:self.active_end:]
         img_lq[1::2] = bottom[20::, self.active_start:self.active_end:]
-        img_lq = np.stack([img_lq])
+        stacked_lq.append(img_lq)
 
-        return {'lq': img_lq, 'gt': img_gt}
+        if chroma_tbc:
+            src = np.fromfile(
+                chroma_tbc,
+                dtype=np.uint16,
+                count=910 * 526,
+                offset=index * 910 * 526 * 2
+            ).astype(np.float32) / UINT16_MAX
+            src = src.reshape(526, 910)
+
+            bottom, top = np.split(src, 2)
+            # Normalize the phase between lines in field
+            bottom[20::2, self.active_start:self.active_end:] = bottom[20::2, self.active_start:self.active_end:] * -1 + 1
+            top[19:262:2, self.active_start:self.active_end:] = top[19:262:2, self.active_start:self.active_end:] * -1 + 1
+
+            img_lq = np.zeros((486, self.active_end - self.active_start), dtype=np.float32)
+            img_lq[0::2] = top[19:262:, self.active_start:self.active_end:]
+            img_lq[1::2] = bottom[20::, self.active_start:self.active_end:]
+            stacked_lq.append(img_lq)
+
+        return {'lq': np.stack(stacked_lq), 'gt': img_gt}
 
     def __len__(self):
         return len(self.gt_list)
@@ -63,26 +107,56 @@ class single_tbc(data.Dataset):
         super(single_tbc, self).__init__()
         self.opt = opt
 
-        self.lq_tbc = Path(opt['tbc'])
-        with open(self.lq_tbc.with_suffix('.tbc.json')) as tbc_json_file:
+        self.lq_tbcs = [Path(tbc) for tbc in opt['tbc']]
+        with open(self.lq_tbcs[0].with_suffix('.tbc.json')) as tbc_json_file:
             tbc_json = json.load(tbc_json_file)
         self.active_start = tbc_json['videoParameters']['activeVideoStart']
         self.active_end = tbc_json['videoParameters']['activeVideoEnd']
         self.frames = opt['frames']
 
     def __getitem__(self, index):
-        bottom, top = np.split(
-            (np.fromfile(
-                self.lq_tbc,
-                dtype=np.uint16,
-                count=910 * 526,
-                offset=self.frames[index] * 910 * 526 * 2).astype(np.float32) / UINT16_MAX
-            ).reshape(526, 910), 2)
+        stacked_lq = []
+        if len(self.lq_tbcs) == 2:
+            luma_tbc, chroma_tbc = self.lq_tbcs
+        else:
+            luma_tbc, = self.lq_tbcs
+            chroma_tbc = None
+
+        src = np.fromfile(
+            luma_tbc,
+            dtype=np.uint16,
+            count=910 * 526,
+            offset=self.frames[index] * 910 * 526 * 2
+        ).astype(np.float32) / UINT16_MAX
+        src = src.reshape(526, 910)
+
+        bottom, top = np.split(src, 2)
+
         img_lq = np.zeros((486, self.active_end - self.active_start), dtype=np.float32)
         img_lq[0::2] = top[19:262:, self.active_start:self.active_end:]
         img_lq[1::2] = bottom[20::, self.active_start:self.active_end:]
-        img_lq = np.stack([img_lq])
-        return {'lq': img_lq, 'lq_path': str(self.lq_tbc.with_stem(f'{self.lq_tbc.stem}_frame_{self.frames[index]}'))}
+        stacked_lq.append(img_lq)
+
+        if chroma_tbc:
+            src = np.fromfile(
+                chroma_tbc,
+                dtype=np.uint16,
+                count=910 * 526,
+                offset=self.frames[index] * 910 * 526 * 2
+            ).astype(np.float32) / UINT16_MAX
+            src = src.reshape(526, 910)
+
+            bottom, top = np.split(src, 2)
+            # Normalize the phase between lines in field
+            bottom[20::2, self.active_start:self.active_end:] = bottom[20::2, self.active_start:self.active_end:] * -1 + 1
+            top[19:262:2, self.active_start:self.active_end:] = top[19:262:2, self.active_start:self.active_end:] * -1 + 1
+
+            img_lq = np.zeros((486, self.active_end - self.active_start), dtype=np.float32)
+            img_lq[0::2] = top[19:262:, self.active_start:self.active_end:]
+            img_lq[1::2] = bottom[20::, self.active_start:self.active_end:]
+            stacked_lq.append(img_lq)
+
+        return {'lq': np.stack(stacked_lq), 'lq_path': str(self.lq_tbcs[0].with_stem(f'{self.lq_tbcs[0].stem}_frame_{self.frames[index]}'))}
 
     def __len__(self):
         return len(self.frames)
